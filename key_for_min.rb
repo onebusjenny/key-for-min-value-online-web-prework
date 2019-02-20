e9840e17ddc5f71253952a26eea593ae8dfb9505
Jenny Chu <jchudesign0930@gmail.com>
2019-02-20 06:05:22 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  name_hash = {:chair => 25, :table => 85, :mattress => 450}
+  name_hash = {:blake => 500, :ashley => 2, :adam => 1}
 
 end
 
